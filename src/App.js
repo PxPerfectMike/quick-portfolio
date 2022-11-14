d06f234278c855336d6ad1718f156ab66dab02f3
michael baker <56899037+PxPerfectMike@users.noreply.github.com>
2022-11-14 14:28:36 -0800

    working no styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,113 @@
+import React from 'react';
 import './App.css';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import ColorLensIcon from '@mui/icons-material/ColorLens';
+import CodeIcon from '@mui/icons-material/Code';
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
 
 function App() {
-	return (
+	const stuffArr = [
 		<>
-			<div></div>
-		</>
+			<ColorLensIcon sx={{ fontSize: '40px' }} />
+			<h2>Designer</h2>
+			<p>
+				I value simple and meaningful experiences, clean design patterns, and
+				thoughtful interactions. Everything I create has thought and purpose
+				behind it and I strive to make the best possible user experience across
+				all projects.
+			</p>
+			<a
+				href='https://pxperfect-quick-chat.herokuapp.com/'
+				target='_blank'
+				rel='noreferrer'
+			>
+				Quick Chat App
+			</a>
+		</>,
+		<>
+			<CodeIcon sx={{ fontSize: '40px' }} />
+			<h2>Frontend Developer</h2>
+			<p>
+				I like to create things from scratch (especially in React), and enjoy
+				bringing interactive ideas to life in the browser using modern
+				techniques. Beautiful and functional design is my passion.
+			</p>
+			<p>Netflix Helper</p>
+			<ul style={{ listStyleType: 'none', paddingInlineStart: 0 }}>
+				<li className='blockList'>
+					<a
+						href='https://play.google.com/store/apps/details?id=com.netflix_helper_app.www.twa&hl=en_US&gl=US&pli=1'
+						target='_blank'
+						rel='noreferrer'
+					>
+						(Google Play Store)
+					</a>
+				</li>
+				{'or'}
+				<li className='blockList'>
+					<a
+						href='https://www.netflix-helper-app.com/'
+						target='_blank'
+						rel='noreferrer'
+					>
+						(PWA Website)
+					</a>
+				</li>
+			</ul>
+		</>,
+		<>
+			<ColorLensIcon />
+			<h2>Designer</h2>
+			<p>
+				I value simple and meaningful experiences, clean design patterns, and
+				thoughtful interactions. Everything I create has thought and purpose
+				behind it and I strive to make the best possible user experience across
+				all projects.
+			</p>
+			<a
+				href='https://pxperfect-quick-chat.herokuapp.com/'
+				target='_blank'
+				rel='noreferrer'
+			>
+				Quick Chat App
+			</a>
+		</>,
+	];
+	return (
+		<Grid
+			sx={{ flexGrow: 1 }}
+			color={'#f5f5f5'}
+			textAlign={'center'}
+			container
+			spacing={2}
+		>
+			<Grid item xs={12}>
+				<h1>Michael Baker Portfolio</h1>
+			</Grid>
+			<Grid item xs={12} md={12}>
+				<Grid
+					flexDirection={'column'}
+					container
+					alignItems='center'
+					spacing={'2vw'}
+				>
+					{[0, 1, 2].map((value) => (
+						<Grid key={value} item>
+							<Paper
+								sx={{
+									height: '35vh',
+									width: '95vw',
+									backgroundColor: '#f5f5f5',
+								}}
+							>
+								{stuffArr[value]}
+							</Paper>
+						</Grid>
+					))}
+				</Grid>
+			</Grid>
+		</Grid>
 	);
 }
 
